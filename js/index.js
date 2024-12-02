0758802a0b2d702e6f526e2616e0088f86b7c9d6
Tais-Crisostomo <taissaboia03@gmail.com>
2024-12-02 16:53:18 -0300

    primeiro código

---
+++ js/index.js
@@ -0,0 +1,21 @@
+async function gerarPokemon(num) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
+    const json = await response.json()
+    return {
+            name: json.name,
+            type1: json.types[0].type.name,
+            type2: json.types[1].type.name,
+            sprite: json.sprites.other.showdown.front_default
+
+        }
+}
+
+
+    async function setPokemonImg(){
+        const card = document.getElementById('card')
+        const h2 = document.getElementById('title')
+        const pokemon = await gerarPokemon(1)
+        h2.textContent = pokemon.name
+        card.setAttribute('src', pokemon.sprite)
+    }
+    setPokemonImg()
